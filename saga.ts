6fc52c6900315e745e020107337d9d3e09141f2b
niboshi <niboshiporipori@gmail.com>
2019-06-10 22:04:15 +0900

    Add saga test (#72)

--- saga.ts
+++ saga.ts
@@ -1,7 +1,3 @@
-/* global fetch */
-
-import es6promise from 'es6-promise';
-import 'isomorphic-unfetch';
 import { all, delay, takeEvery } from 'redux-saga/effects';
 import { bindAsyncAction } from 'typescript-fsa-redux-saga';
 import {
@@ -10,18 +6,20 @@ import {
   IRequestAmountChangingWithSleepPayload
 } from './actions';
 
-es6promise.polyfill();
-
-const counterIncrementWorker = bindAsyncAction(
+export const counterIncrementWorker = bindAsyncAction(
   counterAsyncActionCreators.changeAmountWithSleep
 )(function*(payload: IRequestAmountChangingWithSleepPayload) {
   yield delay(payload.sleep);
   return { amount: payload.amount };
 } as any); // FIXME remove any
 
-function* watchIncrementAsync() {
-  yield takeEvery(counterActionCreators.clickAsyncIncrementButton.type, () =>
-    counterIncrementWorker({ amount: 1, sleep: 1000 })
+export const counterIncrementWorkerWrapper = () =>
+  counterIncrementWorker({ amount: 1, sleep: 1000 });
+
+export function* watchIncrementAsync() {
+  yield takeEvery(
+    counterActionCreators.clickAsyncIncrementButton.type,
+    counterIncrementWorkerWrapper
   );
 }
 
