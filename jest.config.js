0432997438e89224b4ead6153bccff5b09a195b0
niboshi <niboshiporipori@gmail.com>
2019-07-09 15:19:42 +0900

    Use Next.js TypeScript support (#107)
    * Use Next.js TypeScript support
    
    * Add tsconfig for test

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testMatch: ['**/__tests__/**/*.ts?(x)'],
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./setupTests.js']
+  setupFilesAfterEnv: ['./setupTests.js'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json'
+    }
+  }
 };
