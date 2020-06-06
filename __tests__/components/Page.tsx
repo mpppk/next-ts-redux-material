8cbf7a7d5819be93772dcd114d214dad97e11d90
mpppk <niboshiporipori@gmail.com>
2020-06-06 23:19:26 +0900

    Fix test

--- __tests__/components/Page.tsx
+++ __tests__/components/Page.tsx
@@ -1,15 +1,12 @@
 import Typography from '@material-ui/core/Typography/Typography';
 import { shallow } from 'enzyme';
 import * as React from 'react';
-import AppBar from '../../components/AppBar';
 import Counter from '../../components/Counter';
 import Page from '../../components/Page';
 
 describe('Page', () => {
   // tslint:disable-next-line no-empty
   const emptyButtonHandler = (_nc: number) => {};
-  // tslint:disable-next-line no-empty
-  const emptyClickEventHandler = () => {};
 
   const wrapper = shallow(
     <Page
@@ -18,13 +15,8 @@ describe('Page', () => {
       onClickIncrementButton={emptyButtonHandler}
       onClickDecrementButton={emptyButtonHandler}
       onClickIncrementLaterButton={emptyButtonHandler}
-      onClickLogout={emptyClickEventHandler}
-      user={null}
     />
   );
-  it('has 1 AppBar', async () => {
-    expect(wrapper.find(AppBar)).toHaveLength(1);
-  });
   it('has 1 Typography', async () => {
     expect(wrapper.find(Typography)).toHaveLength(1);
   });
