aa6c3e4bb572ad915af39d9537e0e113e69c21ae
niboshi <niboshiporipori@gmail.com>
2020-06-07 22:54:55 +0900

    Add cypress (#372)
    * Add Cypress test
    
    * Add CI setting for Cypress
    
    * Add record key

---
+++ cypress/integration/main_spec.js
@@ -0,0 +1,35 @@
+describe('Increment and Decrement', () => {
+  it('clicking "increment" and "decrement" button changes the count', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('+1').click().click()
+    cy.contains('Count:').children('span')
+      .should('have.text', '2')
+
+    cy.contains('-1').click()
+    cy.contains('Count:').children('span')
+      .should('have.text', '1')
+
+    cy.contains('+1 later').click()
+    cy.contains('Count:').children('span')
+      .should('have.text', '2')
+  })
+})
+
+describe('Show About page', () => {
+  it('show about page', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('button[aria-label="menu"]').click()
+    cy.contains('About').click()
+    cy.url().should('include', '/about')
+    cy.get('h2:first-of-type').should('have.text', 'About')
+  })
+})
+
+describe('Show Sign in page', () => {
+  it('show sign in page', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('Sign In').click()
+    cy.url().should('include', '/signin')
+    cy.get('button').contains('Sign In')
+  })
+})
